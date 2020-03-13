f4c55fd745b0c9d78f65a1b47f392cc5db16efb4
Mohamed essamaali <mohamed.essamaali@gmail.com>
2020-03-13 16:49:17 -0400

    Added Router to Index.js and Route to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import {Route} from 'react-router-dom';
 import SavedList from './Movies/SavedList';
 import { Router } from 'express';
 
@@ -15,7 +15,7 @@ const App = () => {
       <SavedList list={savedList} />
       {/* <div>Replace this Div with your Routes</div> */}
       <Route exact path = "/"> </Route>
-      <Route path= "/movies/id"></Route>
+      <Route path= "/movies/:id"></Route>
     </div>
   );
 };
