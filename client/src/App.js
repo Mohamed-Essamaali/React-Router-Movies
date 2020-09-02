da6ca9764bd1acffca124c467ad1e68d8d524e0f
Mohamed essamaali <mohamed.essamaali@gmail.com>
2020-09-02 17:54:27 -0400

    MVP completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList'
+import Movie from './Movies/Movie'
+import {Route,Link} from 'react-router-dom'
+import MovieCard from './Movies/MovieCard';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -8,11 +12,23 @@ const App = () => {
   const addToSavedList = movie => {
     setSavedList( [...savedList, movie] );
   };
+ console.log('saved movies', savedList)
 
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      
+      {/* <div style={{color:'#151515',textDecoration:'none',background:'lightskyblue', width:'10%',padding:'1%',}}>
+     
+      </div> */}
+      
+      
+      <Route exact  path='/'>
+        <MovieList/>
+      </Route >
+      <Route path={`/movies/:movieId`}>
+        <Movie addToSavedList={addToSavedList}/>
+      </Route>
     </div>
   );
 };
